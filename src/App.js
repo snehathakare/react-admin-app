7d8a3cd0baa7691d78c8972ef4eba22663f270f5
Sneha <snhthakare@gmail.com>
2022-05-24 14:43:21 +0530

    Initial routes setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import { Routes, Route, Link } from "react-router-dom";
+import { Home, Login } from './pages'
 
 function App() {
   return (
     <div className="App">
       react-admin-app
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="login" element={<Login />} />
+      </Routes>
     </div>
   );
 }
